47b27722fc9e7498b3820a24953a064c852af3a4
vrundacvaghasiya <vrundacvaghasiya@gmail.com>
2025-03-19 12:30:51 -0400

    Task:86a7bpb8k UI: Replace "Tender Disovery Platform" black box with search bar

--- apps/frontend/src/components/layouts/Sidebar.tsx
+++ apps/frontend/src/components/layouts/Sidebar.tsx
@@ -8,28 +8,28 @@ const Sidebar = () => {
 
   return (
     <nav className="w-64 bg-gray-800 text-white p-8 space-y-4">
-      <img src={logo} alt="Wouessi Logo" className="w-32 h-auto" />
+      <img src={logo} alt="Wouessi Logo" className="w-32 h-auto mt-10 mb-4" />
       <div className="w-full h-0.5 bg-gradient-to-r from-gray-700 via-gray-500 to-gray-700"></div>
       <div className="flex flex-col justify-between h-[75vh]">
         <div>
           <ul className="space-y-3">
             <li>
-              <Link to="/tenderdata" className="block p-3 bg-gray-700 rounded">
+              <Link to="/tenderdata" className="block p-3 nav-bg-color rounded">
                 Tender Data
               </Link>
             </li>
             <li>
-              <Link to="/lg" className="block p-3 bg-gray-700 rounded">
+              <Link to="/lg" className="block p-3 nav-bg-color rounded">
                 Lead Generation
               </Link>
             </li>
             <li>
-              <Link to="/ca" className="block p-3 bg-gray-700 rounded">
+              <Link to="/ca" className="block p-3 nav-bg-color rounded">
                 Capability Assessment
               </Link>
             </li>
             <li>
-              <Link to="/bm" className="block p-3 bg-gray-700 rounded">
+              <Link to="/bm" className="block p-3 nav-bg-color rounded">
                 Benchmarking
               </Link>
             </li>

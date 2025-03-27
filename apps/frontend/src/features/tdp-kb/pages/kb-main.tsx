a32515da8ff0e114909fb09035bfdacb271ed6e4
Eric Njanga <eric.njanga@gmail.com>
2025-03-27 11:35:26 -0400

    Dummy data created

--- apps/frontend/src/features/tdp-kb/pages/kb-main.tsx
+++ apps/frontend/src/features/tdp-kb/pages/kb-main.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
+import { dummyArticles } from '../components/dummy'
 
 const BmMain = () => {
-
   return (
     <>
-        <h1>Knowledge Base Module</h1>
-
+      <h1>Knowledge Base Module</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {dummyArticles &&
+          dummyArticles.length &&
+          dummyArticles.map((article) => (
+            <p key={article.id}>{article.title}</p>
+          ))}
+      </div>
     </>
   )
 }
